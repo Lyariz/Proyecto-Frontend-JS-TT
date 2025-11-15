84949a6ecdeb08705852765159a3faddb771ac54
Maira Rudolf <rudolf.maira99@gmail.com>
2025-11-15 20:40:54 -0300

    Avance del proyecto: Agrego carpetas js y data, funciones de carrito, mejoras en HTML y CSS

---
+++ js/index.js
@@ -0,0 +1,22 @@
+import {agregarAlCarrito} from "./funcionesCarrito.js";
+import {obtenerCarrito} from "./storage.js";
+import {actualizarContador, mostrarMensaje} from "./ui.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const contenedor = document.getElementById("contenedor-productos");
+    const carrito = obtenerCarrito();
+    actualizarContador(carrito);
+
+    fetch("./data/productos.json").then(res =>{
+        if(!res.ok){
+            throw new Error(`Error HTTP status: ${res.status}`);
+        }
+
+        return res.json()
+    })
+    .then((data) => {
+
+    })
+    .catch(error => console.log(error));
+    
+});
